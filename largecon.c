aab74caddb91b67f144865bc1b5606b7b46f92e6
Deep1704 <95118819+Deep1704@users.noreply.github.com>
2021-12-07 13:09:14 +0530

    Add files via upload

---
+++ largecon.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter a=");
+	scanf("%d",&a);
+	printf("enter b=");
+	scanf("%d",&b);
+	(a>b)?(printf("a is largest no.")):(printf("b is largest no."));
+}
